0fb1d31cf5ec7cfaf45fd6a3972856bcf9ec8c5f
Paul Hatchman <phatchman@gmail.com>
2023-02-19 07:58:14 +1100

    Fixes to build

--- add_git_submodule.cmake
+++ add_git_submodule.cmake
@@ -13,7 +13,7 @@ find_package(Git REQUIRED)
 
 if(NOT EXISTS ${dir}/CMakeLists.txt)
   execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     COMMAND_ERROR_IS_FATAL ANY)
 endif()
 
@@ -32,7 +32,7 @@ find_package(Git REQUIRED)
 
 if(NOT EXISTS ${dir}/CMakeLists.txt)
   execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init -- ${dir}
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     COMMAND_ERROR_IS_FATAL ANY)
 endif()
 
