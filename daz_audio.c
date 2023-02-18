9daf2a817c65431abd049499e1cfd2081e53cd8f
Paul Hatchman <phatchman@gmail.com>
2023-02-18 20:26:41 +1100

    Added delay processing to audio. Audio now working as expected.

--- daz_audio.c
+++ daz_audio.c
@@ -14,27 +14,37 @@
 #include "pico/binary_info.h"
 #include "hardware/pio.h"
 
+#define DEBUG
+
 #define audio_pio __CONCAT(pio, PICO_AUDIO_I2S_PIO)
 bi_decl(bi_3pins_with_names(PICO_AUDIO_I2S_DATA_PIN, "I2S DIN", PICO_AUDIO_I2S_CLOCK_PIN_BASE, "I2S BCK", PICO_AUDIO_I2S_CLOCK_PIN_BASE+1, "I2S LRCK"));
 bool play_audio_sample_cb(struct repeating_timer *t);
 
-static volatile uint32_t current_audio_sample = 0;
-static struct audio_buffer_pool *producer_pool = 0;
-static alarm_pool_t *audio_alarm_pool = 0;
+static uint32_t current_sample = 0;
+static bool have_delay[2];
+static uint16_t next_sample[2];
+static absolute_time_t next_delay[2];
+
+static alarm_pool_t *audio_alarm_pool;
+static queue_t chan0_queue;
+static queue_t chan1_queue;
+static audio_buffer_pool_t *producer_pool;
 
 void audio_init() {
     static repeating_timer_t audio_timer;
 
-    static audio_format_t audio_format = {
-            .format = AUDIO_BUFFER_FORMAT_PCM_S16,
-            .sample_freq = 48000,
-            .channel_count = 2,
+    static audio_format_t audio_format = 
+    {
+        .format = AUDIO_BUFFER_FORMAT_PCM_S16,
+        .sample_freq = 48000,
+        .channel_count = 2,
     };
 
-    static struct audio_buffer_format producer_format = {
-            .format = &audio_format,
-            .sample_stride = 2
-        };
+    static struct audio_buffer_format producer_format = 
+    {
+        .format = &audio_format,
+        .sample_stride = 2
+    };
 
     producer_pool = audio_new_producer_pool(&producer_format, 16, 1); // todo correct size
     bool __unused ok;
@@ -57,6 +67,8 @@ void audio_init() {
     ok = audio_i2s_connect(producer_pool);
     assert(ok);
     audio_i2s_set_enabled(true);
+    queue_init(&chan0_queue, 4, 512);
+    queue_init(&chan1_queue, 4, 512);
     audio_alarm_pool = alarm_pool_create(2, 2);
 
 //    add_repeating_timer_us(-21, play_audio_sample_cb, NULL, &audio_timer);
@@ -65,10 +77,66 @@ void audio_init() {
     alarm_pool_add_repeating_timer_us(audio_alarm_pool,-20, play_audio_sample_cb, NULL, &audio_timer);
 }
 
+bool __time_critical_func(play_audio_sample_cb)(struct repeating_timer *t) 
+{
+    absolute_time_t current_time = get_absolute_time();
 
-bool play_audio_sample_cb(struct repeating_timer *t) {
-//    printf("Play: %X\n", current_audio_sample);
-    pio_sm_put(audio_pio, 3, current_audio_sample);
+    if (absolute_time_diff_us(next_delay[0], current_time) >= 0)
+    {
+        current_sample = (current_sample & 0xffff00000) | next_sample[0];
+
+        uint32_t delay_and_sample;
+        if (queue_try_remove(&chan0_queue, &delay_and_sample))
+        {
+            next_delay[0] = delayed_by_us(current_time, delay_and_sample >> 16);
+            next_sample[0] = delay_and_sample & 0x0000ffff;
+            have_delay[0] = true;
+        }
+        else
+        {
+            have_delay[0] = false;
+            current_sample &= 0xffff0000;
+        }
+    }
+    else if (!have_delay[0])
+    {
+        uint32_t delay_and_sample;
+        if (queue_try_remove(&chan0_queue, &delay_and_sample))
+        {
+            have_delay[0] = true;
+            next_delay[0] = delayed_by_us(current_time, 5000);
+            next_sample[0] = delay_and_sample & 0x0000ffff;
+        }
+    }
+
+    if (absolute_time_diff_us(next_delay[1], current_time) >= 0)
+    {
+        current_sample = (current_sample & 0x0000ffff) | (next_sample[1]<< 16);
+
+        uint32_t delay_and_sample;
+        if (queue_try_remove(&chan1_queue, &delay_and_sample))
+        {
+            next_delay[1] = delayed_by_us(current_time, delay_and_sample >> 16);
+            next_sample[1] = delay_and_sample & 0x0000ffff;
+            have_delay[1] = true;
+        }
+        else
+        {
+            have_delay[1] = false;
+            current_sample &= 0x0000ffff;
+        }
+    }
+    else if (!have_delay[1])
+    {
+        uint32_t delay_and_sample;
+        if (queue_try_remove(&chan1_queue, &delay_and_sample))
+        {
+            have_delay[1] = true;
+            next_delay[1] = delayed_by_us(current_time, 5000);
+            next_sample[1] = delay_and_sample & 0x0000ffff;
+        }
+    }
+    pio_sm_put(audio_pio, 3, current_sample);
     return true;
 }
 
@@ -76,22 +144,26 @@ void audio_add_sample(uint8_t channel, uint16_t delay_us, uint8_t sample)
 {
     if (channel == 0)
     {
-        uint32_t new_sample = current_audio_sample & 0x0000ffff;
-        new_sample |= (sample << 24);
-//        printf("New Sample: %X:%X:%X:%X\n", sample, (uint32_t) sample << 24, new_sample, current_audio_sample);
-        current_audio_sample = new_sample;
+        uint32_t value = (delay_us << 16) | (sample << 8);
+        if (queue_try_add(&chan0_queue, &value) == false)
+        {
+#ifdef DEBUG
+            printf("Chan0 audio queue full\n");
+#endif
+        }
     }
     else
     {
-        uint32_t new_sample = current_audio_sample & 0xffff0000;
-        new_sample |= (sample << 8);
-//        printf("New Sample: %X:%X:%X\n", sample, new_sample, current_audio_sample);
-        current_audio_sample = new_sample;
+        uint32_t value = (delay_us << 16) | (sample << 8);
+        if (queue_try_add(&chan1_queue, &value) == false)
+        {
+#ifdef DEBUG
+            printf("Chan1 audio queue full\n");
+#endif
+        }
     }
     return;
 }
-
-
 
 #if DAZAUDIO_STANDALONE
 void audio_add_sample(uint8_t channel, uint16_t delay_us, uint8_t sample);
