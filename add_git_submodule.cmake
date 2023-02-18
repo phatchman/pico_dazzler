40652ff421d05f176e7602b05101e06c4dbe80ce
Paul Hatchman <phatchman@gmail.com>
2023-02-19 08:19:46 +1100

    fixed to enable build on Raspberry PI

--- add_git_submodule.cmake
+++ add_git_submodule.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.19)
+cmake_minimum_required(VERSION 3.13)
 
 function(add_git_submodule_recurse dir)
 # add a Git submodule directory to CMake, assuming the
@@ -10,11 +10,17 @@ function(add_git_submodule_recurse dir)
 # add_git_submodule_recurse(mysubmod_dir)
 
 find_package(Git REQUIRED)
-
-if(NOT EXISTS ${dir}/CMakeLists.txt)
-  execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND_ERROR_IS_FATAL ANY)
+if (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} GREATER 3.18)
+  if(NOT EXISTS ${dir}/CMakeLists.txt)
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
+      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+      COMMAND_ERROR_IS_FATAL ANY)
+  endif()
+else()
+  if(NOT EXISTS ${dir}/CMakeLists.txt)
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+  endif()
 endif()
 
 endfunction(add_git_submodule_recurse)
@@ -30,10 +36,17 @@ function(add_git_submodule dir)
 
 find_package(Git REQUIRED)
 
-if(NOT EXISTS ${dir}/CMakeLists.txt)
-  execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init -- ${dir}
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND_ERROR_IS_FATAL ANY)
+if (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} GREATER 3.18)
+  if(NOT EXISTS ${dir}/CMakeLists.txt)
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init -- ${dir}
+      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+      COMMAND_ERROR_IS_FATAL ANY)
+  endif()
+else ()
+  if (NOT EXISTS ${dir}/CMakeLists.txt)
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init -- ${dir}
+      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+  endif()
 endif()
 
 endfunction(add_git_submodule)
