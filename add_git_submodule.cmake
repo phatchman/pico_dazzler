7aeeb72a7c4681edb252355f61536d1db9fece99
Paul Hatchman <phatchman@gmail.com>
2023-02-19 06:16:35 +1100

    Updated build environment to pull right submodules Updated tinyusb to fork that supports Due

---
+++ add_git_submodule.cmake
@@ -0,0 +1,39 @@
+cmake_minimum_required(VERSION 3.19)
+
+function(add_git_submodule_recurse dir)
+# add a Git submodule directory to CMake, assuming the
+# Git submodule directory is a CMake project.
+#
+# Usage: in CMakeLists.txt
+# 
+# include(AddGitSubmodule.cmake)
+# add_git_submodule_recurse(mysubmod_dir)
+
+find_package(Git REQUIRED)
+
+if(NOT EXISTS ${dir}/CMakeLists.txt)
+  execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    COMMAND_ERROR_IS_FATAL ANY)
+endif()
+
+endfunction(add_git_submodule_recurse)
+
+function(add_git_submodule dir)
+# add a Git submodule directory to CMake, assuming the
+# Git submodule directory is a CMake project.
+#
+# Usage: in CMakeLists.txt
+# 
+# include(AddGitSubmodule.cmake)
+# add_git_submodule(mysubmod_dir)
+
+find_package(Git REQUIRED)
+
+if(NOT EXISTS ${dir}/CMakeLists.txt)
+  execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init -- ${dir}
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    COMMAND_ERROR_IS_FATAL ANY)
+endif()
+
+endfunction(add_git_submodule)
